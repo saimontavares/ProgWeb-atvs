42a7b36c108f765ecf926db110de5395a53f1009
Saimon Tavares <48137085+saimontavares@users.noreply.github.com>
2025-05-29 22:05:26 +0000

    node 2

---
+++ node1/src/index.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs')
+const dotenv = require('dotenv')
+const { createLink } = require('./util')
+
+dotenv.config({path:`.env.${process.env.NODE_ENV}`})
+
+const PORT = process.env.PORT ?? 3333;
+const DIR = process.argv[2] ?? 'public'
+
+const server = http.createServer(function(req, res) {
+    output = ''
+    abreArquivo = false
+    res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+    fs.readdir(DIR, (err, files) => {
+        if (err) {
+            res.write('erro')
+            res.end()
+            return;
+        }
+        else{
+            files.forEach(file => {
+                /* res.write(`${file}<br>`); */
+                if(req.url.endsWith(file)){
+                    abreArquivo = true
+                    fs.readFile(DIR+req.url, (error, f)=> {
+                        if (error) {
+                            res.end()
+                            return;
+                        }
+                        else{
+                            output = '<a href="/">Voltar</a><br><p>'+f.toString()+'</p>'
+                            res.write(output)
+                            res.end()
+                            return;
+                        }
+                    })
+                }
+                if(abreArquivo){
+                    return
+                }
+                else{
+                    output += createLink(file);
+                }
+            });
+            if(!abreArquivo){
+                res.write(output)
+                res.end();
+            }
+        }
+    })
+});
+
+server.listen(PORT);
---
+++ node1/src/util.js
@@ -0,0 +1,5 @@
+function createLink(filename) {
+    return `<a href="/${filename}">${filename}</a><br>\n`;
+}
+
+module.exports = { createLink };
