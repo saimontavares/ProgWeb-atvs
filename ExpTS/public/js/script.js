39f0f199ce51cb24057c25cebd7adf4ad54a8040
Saimon Tavares <48137085+saimontavares@users.noreply.github.com>
2025-06-30 21:30:05 +0000

    modal funcionando no major, mas o botão de exlusão não funciona

--- ExpTS/public/js/script.js
+++ ExpTS/public/js/script.js
@@ -1,1 +1,15 @@
-console.log("oi")
+function deletarMajor(majorIdToDelete) {
+    fetch(`/major/remove/${majorIdToDelete}`, { method: 'POST' })
+        .then(response => {
+            if (response.ok) {
+                console.log('Major deletado com sucesso');
+                window.location.reload();
+            } else {
+                console.log('Erro ao deletar');
+            }
+        });
+}
+
+$('#exampleModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
